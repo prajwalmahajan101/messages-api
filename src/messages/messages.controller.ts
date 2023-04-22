2316e7243693069856af297c0d107957c0d235af
Prajwal Mahajan <82263793+prajwalmahajan101@users.noreply.github.com>
2023-04-22 21:49:40 +0530

    controller added with decorator

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,8 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 
 // Created with the cli command
 // nest g controller messages/messages --flat
 // flat flag tells to not create am extra folder
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+  @Get()
+  listMessages() {
+    return { msg: 'hello' };
+  }
+
+  @Post()
+  createMessage(@Body() body: any) {
+    return body;
+  }
+
+  @Get('/:id')
+  getMessage(@Param('id') id: string) {
+    return { msg: id };
+  }
+}
